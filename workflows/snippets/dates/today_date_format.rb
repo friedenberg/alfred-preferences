22cb59bb8f2fd88663fa75312fa458e9dd082bc3
Sasha Friedenberg <sashafriedenberg@gmail.com>
2020-11-13 21:40:29 -0500

    reformat today_date_format.rb

--- workflows/snippets/dates/today_date_format.rb
+++ workflows/snippets/dates/today_date_format.rb
@@ -23,11 +23,11 @@ TimeFormat = Struct.new(:title, :value, :identifier) do
 end
 
 FORMATS = [
-	TimeFormat.new("Week Number", 			`date +%V`, 							"snippets.date.week_number"),
-	TimeFormat.new("Time Zone", 				`date +%Z`, 							"snippets.date.time_zone"),
-	TimeFormat.new("UTC", 							`date +%s`, 							"snippets.date.timestamp"),
-	TimeFormat.new("File Safe Date", 	`date +%Y-%m-%d`, 				"snippets.date.file_safe_date"),
-	TimeFormat.new("Daily Log Date", 	`date '+%Y-%m-%d, %A'`, 	"snippets.date.daily_log_date"),
+	TimeFormat.new("Week Number",    `date +%V`, 							"snippets.date.week_number"),
+	TimeFormat.new("Time Zone",      `date +%Z`, 							"snippets.date.time_zone"),
+	TimeFormat.new("UTC",            `date +%s`, 							"snippets.date.timestamp"),
+	TimeFormat.new("File Safe Date", `date +%Y-%m-%d`, 				"snippets.date.file_safe_date"),
+	TimeFormat.new("Daily Log Date", `date '+%Y-%m-%d, %A'`, 	"snippets.date.daily_log_date"),
 ]
 
 puts ({:items => FORMATS}.to_json)
